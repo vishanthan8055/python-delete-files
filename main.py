0e0962a7bd190e61e2b4db9e35e1ca7a7d939a2d
Vishanthan <vishanthan8055@gmail.com>
2022-01-12 15:56:51 +0530

    Finish code for a single directory

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+path=input("Enter Path: ")
+lst = os.listdir(path)
+
+
+sub= input("enter substring: ")
+print(lst)
+
+for i in range(len(lst)):
+    s = lst[i]
+    ran = len(s) - len(sub)
+    for i in range(ran + 1):
+        if s[i:i + (len(sub))] == sub:
+            print(s)
+            fpath = path + '/' + s
+            os.remove(fpath)
