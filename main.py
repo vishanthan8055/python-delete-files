1b97c9d43df245bbb005cdf62fb0995f9d0f3df3
Vishanthan <vishanthan8055@gmail.com>
2022-01-12 19:40:11 +0530

    modified main.py file

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 
+def delete(sub):
+    for i in range(len(lst)):
+        s = lst[i]
+        ran = len(s) - len(sub)
+        for i in range(ran + 1):
+            if s[i:i + (len(sub))] == sub:
+                fpath = path + '/' + s
+                os.remove(fpath)
+                print(s,"is deleted sucessfully")
+
 path=input("Enter Path: ")
 lst = os.listdir(path)
-
-sub= input("enter substring: ")
 print(lst)
-
-for i in range(len(lst)):
-    s = lst[i]
-    ran = len(s) - len(sub)
-    for i in range(ran + 1):
-        if s[i:i + (len(sub))] == sub:
-            print(s)
-            fpath = path + '/' + s
-            os.remove(fpath)
+sub= input("enter substring: ")
+delete(sub)
