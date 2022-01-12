e56dada884e9a3dc31726498ddbe0b6c58c3f4e9
Vishanthan <vishanthan8055@gmail.com>
2022-01-12 21:04:00 +0530

    final edition

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def delete(sub):
         ran = len(s) - len(sub)
         for i in range(ran + 1):
             if s[i:i + (len(sub))] == sub:
-                fpath = path + '/' + s
+                fpath = root + '/' + s
                 os.remove(fpath)
-                print(s,"is deleted sucessfully")
+                print(s)
 
 # driver code
-path=input("Enter Path: ")
-lst = os.listdir(path)
-print(lst)
-sub= input("enter substring: ")
-delete(sub)
+path = input("Enter Path: ")
+sub = input("enter substring: ")
+print("Deleted Files: ")
+for (root,dirs,files) in os.walk(path):
+        lst=files
+        if len(lst) != 0:
+                delete(sub)
+
